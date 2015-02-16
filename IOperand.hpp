90d3128f076e7bbdc23bda4a76577f2fe5f13ec6
Arthur Poulet <arthur.poulet@cryptolab.net>
2015-02-16 17:02:25 +0100

    Add IOperand.hpp

---
+++ IOperand.hpp
@@ -0,0 +1,20 @@
+#ifndef IOPERAND_H_
+# define IOPERAND_H_
+
+class IOperand
+{
+public:
+  virtual std::string const & toString() const = 0; // Renvoie une string reprensentant l’instance
+  virtual int getPrecision() const = 0; // Renvoie la precision du type de l’instance
+  virtual eOperandType getType() const = 0; // Renvoie le type de l’instance. Voir plus bas
+
+  virtual IOperand * operator+(const IOperand &rhs) const = 0;
+  virtual IOperand * operator-(const IOperand &rhs) const = 0;
+  virtual IOperand * operator*(const IOperand &rhs) const = 0;
+  virtual IOperand * operator/(const IOperand &rhs) const = 0;
+  virtual IOperand * operator%(const IOperand &rhs) const = 0;
+  virtual ~IOperand() {}
+};
+
+
+#endif /* !IOPERAND_H_ */
