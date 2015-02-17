53f8fc36a06502b91409e4387284a869d436c796
Arthur Poulet <arthur.poulet@cryptolab.net>
2015-02-17 03:05:34 +0100

    initialize the arguments and instructions lists of the "parseur"

--- Parser.cpp
+++ Parser.cpp
@@ -1,6 +1,22 @@
 #include "Parser.hpp"
 
 Parser::Parser() {
+  _instructions["push"] =  1;
+  _instructions["pop"] =  0;
+  _instructions["dump"] =  0;
+  _instructions["assert"] = 1;
+  _instructions["add"] =  0;
+  _instructions["sub"] =  0;
+  _instructions["mul"] =  0;
+  _instructions["div"] =  0;
+  _instructions["mod"] =  0;
+  _instructions["print"] =  0;
+  _instructions["exit"] =  0;
+  _arguments["int8"] =  Integer;
+  _arguments["int16"] =  Integer;
+  _arguments["int32"] =  Integer;
+  _arguments["float"] =  Decimal;
+  _arguments["double"] =  Decimal;
 }
 
 Parser::~Parser() {
@@ -29,4 +45,9 @@ void		Parser::loadFile(const std::string& filename)
       return;
     //throw
   }
+}
+
+void		Parser::executeInstruction(const std::string & instruction)
+{
+
 }
--- Parser.hpp
+++ Parser.hpp
@@ -3,9 +3,12 @@
 
 # include <iostream>
 # include <fstream>
+# include <map>
 # include "MutantStack.hpp"
 
 # define BUFF_SIZE 128
+
+enum eArgumentType {Integer, Decimal};
 
 class Parser
 {
@@ -17,9 +20,12 @@ public:
 
   void		parse();
   void		loadFile(const std::string &);
+  void		executeInstruction(const std::string &);
 private:
   MutantStack<std::string> _mutantStack;
   std::istream *is;
+  std::map<std::string, unsigned int> _instructions;
+  std::map<std::string, eArgumentType> _arguments;
 };
 
 #endif
