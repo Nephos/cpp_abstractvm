3b995904c92a7721a5899a71b7bc4f31c1df684b
Arthur Poulet <arthur.poulet@cryptolab.net>
2015-02-17 03:33:54 +0100

    add some throw to chipset (parser)

--- Parser.cpp
+++ Parser.cpp
@@ -33,11 +33,10 @@ Parser::Parser() {
 Parser::~Parser() {
   std::ifstream *tmp;
   if ((tmp = dynamic_cast<std::ifstream *>(_is))) {
-    // A CHANGER
-    if (!tmp)
-      throw 1;
-    tmp->close();
-    delete _is;
+    if (tmp) {
+      tmp->close();
+      delete _is;
+    }
   }
 }
 
@@ -49,7 +48,7 @@ void		Parser::push(IOperand *elem) {
 }
 
 void		Parser::assert(IOperand *elem) {
-  
+
 }
 
 void		Parser::pop()
@@ -92,7 +91,7 @@ void		Parser::parse() {
   char buffer[BUFF_SIZE];
 
   while (_is->getline(buffer, BUFF_SIZE)) {
-    
+
     std::cout << buffer << std::endl;
   }
 }
@@ -100,9 +99,8 @@ void		Parser::parse() {
 void		Parser::initIO(const std::string& filename) {
   std::ifstream * ifs = new std::ifstream(filename.data());
   if (!ifs->is_open())
-    return;
+    throw IOException(std::string("Unable to open ") + filename);
   _is = ifs;
-  //throw
 }
 
 void		Parser::initIO() {
